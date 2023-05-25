9da24ef0d81df60845419f99317488a100831ccd
Dylan <74881467+D7x8@users.noreply.github.com>
2023-05-25 22:15:47 +1200

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,99 @@
+var twitter = "https://www.twitter.com/D7x8_/";
+var password = "LET_ME_IN_MOTHERFUCKER";
+//var linkedin = "";
+var github = "https://github.com/D7x8/";
+
+whois = [
+  "<br>",
+  "Hello there, I see you have found my website, allow me to introduce myself :)",
+  "I am known online as D7x8, I thought of my psydonym by using my brain as well",
+  "as wanting a 4 letter username on websites.",
+  
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "You are a person using this website.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'github         <a href="' + github + '" target="_blank">github/forrestknight' + "</a>",
+  'More coming soon hopefully...',
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">Real-Moment</span>           Please dont run it for the love of god',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  '<span class="command">Real-Moment</span>           Look at my github motherfucker.',
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is the owner of this site?',
+  '<span class="command">whoami</span>         Who are you in case you don\'t know?',
+  '<span class="command">social</span>         Links to my socials',
+  '<span class="command">secret</span>         Thats a secret :)',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           Run this if you are stuck or bc why not?',
+  '<span class="command">clear</span>          Clear this page',
+  '<span class="command">banner</span>         Does the thing :',
+  "<br>",
+];
+
+banner = [
+                                                                                                                  
+"                         . /                                                     /",                              
+"                                                                                 /",                              
+"                    (@@@@@(/                                              *     @@@@@@#",                         
+"                @@@@@@@@#        /                  *,,,*               * ,       #@@@@@@@@",                     
+"            .@@@@@              .          %%%%%%%%%%%%%%%%%%%%%&    ,               *   @@@@@/",                 
+"           @@@@                       @%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  .                    @@@(",                
+"         @@@                        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                     ,  @@@.",             
+"       @@@                        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                      *@@(",            
+"      @@&                       %%%%%%%%&%%%%%%%%%%%%%%%%%%%%%%&%%%%%%%%%%%                       @@@",           
+"     @@ .                      %%%%%%%@%%%%%%%%%%%%%%%%%%%%%@%%%@%%%%%.%%%%%%                       @@,",         
+"   #@@                        %%%%%%%@%%%%%%%%%%%%%%%%%%%%%%@&%%%@@%%%% %%%%%%                     * @@.",        
+"   *@                        %%%%%%%@@%%%%%%%%%%%%%%%@@@%%%%@@@%%%&@%%%% %%%%%%                       @",         
+"  .@                        %%%%%%%@@%%%%,........@%%%%%%%%%&@@@%%%%@@%%%  %%%%%                       @",        
+"  ,                        %%%%%%%@@@%*................*@%%%%%%%%%%%%&@&%%, *%%%%",                               
+"                          .%%%%%%@@@@@@#....................#@%%%%%@@%%%@%%%  %%%%                     .",        
+"                          %&%%%%@@@@%@@@.........................@%%%%%%@@@%%   %%%",                             
+"                         & %%%%%@@%%&@%%/....@@..............@@......@%%@@@%%%    %",                             
+"                          ,%%%%@@%%%%%%%@&..%...,................*@@@@%%%%%%%%%(   ,",                            
+"                          %%%%%@%%%%%%%@@@@@@@@@%..../@....@@@@@@@@@@@@&%%%%%%%%%,",                             
+"                         %%%%%%%%%%%%@@@@@@@@@@@@@@#....@@@@@@@@@@@@@@@@.%%%%%%%%%%%",                            
+"                        /%%%%%%%%%%*@@@@@@@@@@@@@@@@,..&@@@@@@@@@@@@@@@@.@@%%%%%%%%%%%",                          
+"                        %%%%%%%%%.@..*@@@@@@@@@@@@@*....%@@@@@@@@@@@@@....,&%%%%%%%%%%%",                         
+"                       %%%%%&%%@@..............@*...@@@@...&@............%%%%%%%%%.%%%%%",                        
+"                       %%%%@%@@@@@@((#&@@&*.......&@@@@@@,.......(@@@&#(%@@%%%%@%%%%%% .",                        
+"                      %%%@%@@@@@@@@@@@@@@@@*.%....@@@@@@@@....@.&@@@@@@@@@@@@%%%%@%%%%%%%",                       
+"                      %@@%%@@@@@@@@@@@@@@@@@,..@../@@..@@......&@@@@@@@@@@@@@@@&%%@%%%%%%",                       
+"                       %%@@@%%%%%%%%%@@@@@@@...........(.&......@@@@@@@@%%%%%%%%%@@%%%@%",                        
+"                       %@@@@@@@@@@@@@@@@@@@@@.@.&..........*.@.@@@@@@@@@@@@@@@@@@@@@@@@@%%",                      
+"                    %%%@@@@@@@%%%%@@@@@@@@@@@@@.@.&..,..*,#@&@@@&%%%%&@@@@@@@@@@@@@@@@@@&%%%",                    
+"                  %%%&@@@@%%%%%%%%%%%%@@@@@@@@@@@@@@@@@@@@@@%%%%%%%%%%%%%%%@@@@@@@@@@@@@@%%%%%",                  
+"                %%%%%@@@%%%@   %%%%%%%%%%@@@@@@@@@@@@@@@@%%%%%%%%%%%%%%%%%%%%%%@%%@@@@@&%%%%%%%%",                
+"               %%%%%%%,           %%%%%%%%%%@@@@@@@@@@%%%%%              %%%%%%%%&@%%%%%%%%%%%%%%",               
+"              %%%#                   %%%%%%%%%%%&@%%%%%                        #%%%%%%%%%%%%%%",                  
+"                                         #%%%%%%%#",                                                              
+//"                                        ██████  ███████ ██   ██  █████", 
+//"                                        ██   ██      ██  ██ ██  ██   ██", 
+//"                                        ██   ██     ██    ███    █████", 
+//"                                        ██   ██    ██    ██ ██  ██   ██", 
+//"                                        ██████     ██   ██   ██  █████",                                                                                  
+//"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+
+  '<span class="color2">Yes this is an interactive website, and yes you can play around with it :).</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
