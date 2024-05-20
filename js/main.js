dcd5aa53fee62dd1404f3f1a5f64338af11c11e9
Dylan <cyberdylan.purge278@passinbox.com>
2024-05-20 12:55:05 +1200

    fixed more errors

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ function enterKey(e) {
     if (e.keyCode == 13) {
       commands.push(command.innerHTML);
       git = commands.length;
-      addLine("guest@Cyber-Dylan.github.io:$ ~" + command.innerHTML, "no-animation", 0);
+      addLine("guest@cyber-dylan.github.io:$ ~" + command.innerHTML, "no-animation", 0);
       commander(command.innerHTML.toLowerCase());
       command.innerHTML = "";
       textarea.value = "";
